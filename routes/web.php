5cd05c7089f5ee503805f201a24b9b56bca86448
XesIhma <xesihma@o2.pl>
2023-07-30 22:38:15 +0200

    app.blade created - header with logo and nav with 4 inks

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/account', function () {
+    return view('account');
+})->name('account');
 
 Auth::routes();
 
