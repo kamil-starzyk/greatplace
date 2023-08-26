362b66101310f64867129fabdaf7fc52c248ebb3
XesIhma <xesihma@o2.pl>
2023-08-27 01:33:23 +0200

    created roller on home page

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,5 +8,19 @@ $(document).ready(function(){
     $(".slogan").toggleClass('active');
     $(".search-box").toggleClass('active');
 	});
-	
+  $('.pressable').on('click', function(){
+    $('.pressable').removeClass('pressed');
+    $(this).toggleClass('pressed');
+	});
+  $('.la-heart').on('click', function(){
+    $(this).toggleClass('liked');
+    var counter = $(this).closest('.right-span').find('.likes-counter');
+    var likesCount = counter.text();
+    if ($(this).hasClass("liked")) 
+      likesCount++;
+    else likesCount--;
+
+    counter.text(likesCount);
+	});
+
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,26 +13,55 @@
           
           <div class="filters">
             <div class="button-round">Filtry</div>
-            <div class="button-round">Tarnów <i class="las la-angle-down"></i></div>
-            <div class="button-round">+40km <i class="las la-angle-down"></i></div>
-            <div class="button-round">Popularne</div>
+            <select name="city" id="" class="button-round">
+              <option value="tarnow">Tarnów</option>
+              <option value="krakow">Kraków</option>
+              <option value="kasna">Kąśna</option>
+              <option value="tuchow">Tuchów</option>
+              <option value="biecz">Biecz</option>
+            </select>
+            <select name="radius" id="" class="button-round">
+              <option value="2"> +2km </option>
+              <option value="5"> +5km </option>
+              <option value="10"> +10km </option>
+              <option value="15"> +15km </option>
+              <option value="25"> +25km </option>
+              <option value="40"> +40km </option>
+            </select>
+            <div class="button-round pressable" id="popular-button">Popularne</div>
           </div>
         </form>
       </div>
       <div class="content-box">
-        <h2>Popularne</h2>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
-        <div style="height: 100px; border: 2px solid black">Benc</div>
+        <h2 style="margin-bottom: 24px">Popularne</h2>
+        <div class="categories">
+          <div class="button-round pressable">Plener</div>
+          <div class="button-round pressable">Wewnątrz</div>
+          <div class="button-round pressable">Miasto</div>
+          <div class="button-round pressable">W studiu</div>
+        </div>
+        <div class="propositions">
+          <div class="roller-wrap">
+            <div class="roller-body">
+              <div class="roller-card">
+                <img src="{{ asset('img/uploaded_photos/rzepak.jpeg') }}" alt="pole rzepaku">
+                <h5>Pole Rzepaku</h5>
+                <div class="city-n-likes">
+                  <span><i class="las la-map-marker"></i>Tarnów</span>
+                  <span class="right-span"><span class="likes-counter">45</span> <i class="las la-heart"></i></span>
+                </div>
+              </div>
+              <div class="roller-card">
+                <img src="{{ asset('img/uploaded_photos/jabloni.jpeg') }}" alt="pole rzepaku">
+                <h5>Sad Jabłoni</h5>
+                <div class="city-n-likes">
+                  <span><i class="las la-map-marker"></i>Tuchów</span>
+                  <span class="right-span"><span class="likes-counter">23</span> <i class="las la-heart"></i></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
   </div>
 @endsection
