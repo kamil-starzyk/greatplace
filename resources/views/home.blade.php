e7f7c40c0dc4a7d8b46429f53583277803e85477
XesIhma <kamil_starzyk@outlook.com>
2024-06-10 22:48:17 +0200

    styles and grid for Strona Glowna finished

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,32 @@
 @extends('layout')
 
 @section('content')
-    HOME
+
+<div id="main-wrapper">
+
+    <div id="categories">
+        <div class="category-item btn-round btn-black"><span>Wszystkie</span></div>
+        <div class="category-item btn-round"><span>Architektura</span></div>
+        <div class="category-item btn-round"><span>Miejskie</span></div>
+        <div class="category-item btn-round"><span>Krajobrazy naturalne</span></div>
+    </div>
+    
+    <div id="suggested-wrapper">
+        @for ($i = 0; $i < 16; $i++)
+        <div class="card-suggested">
+            <div class="card-image">
+                <img src="{{ asset('img/uploaded/sad_jabloni/thumbnail.jpg') }}" alt="">
+            </div>
+            <div class="card-details">
+                <h3>Sad jabłoni</h3>
+                <p class="place-localisation">    
+                    <img src="{{ asset('img/icons/map_pin.png') }}" class="map_pin_icon" alt=""> <span>Ciężkowice</span>
+                </p>
+            </div>
+        </div>
+        @endfor
+    </div>
+        
+</div>
+
 @endsection
