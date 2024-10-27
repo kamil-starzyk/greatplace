ddc8337f5fc8f100c1c48ad723b8b481b1992395
XesIhma <kamil_starzyk@outlook.com>
2024-10-27 19:45:51 +0100

    authenticated sesion redirects to previous url

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,16 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+//vue
+
+import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import ThreeStepForm from './components/ThreeStepForm.vue';
+app.component('three-step-form', ThreeStepForm);
+
+
+app.mount('#app');
