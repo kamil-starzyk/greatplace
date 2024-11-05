ce372bf88afcaa065294001847c6192f6623a55d
XesIhma <kamil_starzyk@outlook.com>
2024-11-05 21:14:04 +0100

    installed lang directory with pl and en

---
+++ lang/pl/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Poniższe linie językowe są używane podczas uwierzytelniania dla różnych
+    | komunikatów, które musimy wyświetlić użytkownikowi. Możesz je modyfikować
+    | zgodnie z wymaganiami swojej aplikacji.
+    |
+    */
+
+    'failed' => 'Podane login lub hasło są nieprawidłowe',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Zbyt wiele prób logowania. Spróbuj ponownie za :seconds sekund.',
+
+];
