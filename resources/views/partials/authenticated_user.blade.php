3416c9b3c8c88d06535977ddfc0c9ba361f52165
XesIhma <kamil_starzyk@outlook.com>
2024-12-16 23:35:45 +0100

    styled profile_menu div

--- resources/views/partials/authenticated_user.blade.php
+++ resources/views/partials/authenticated_user.blade.php
@@ -1,11 +1,32 @@
 <div id="user_profile_photo">
   <img src="{{ asset('img/profile_photo.jpg') }}" alt="" onclick="toggleMenu()">
   <div id="profile_menu" style="display:none">
-    <a href="/personal-data">Profil</a>
-    <form method="POST" action="{{ route('logout') }}">
-    @csrf
-      <input type="submit" value="Wyloguj">
-    </form>
+    <ul id="profile_menu_items">
+      <li class="profile_menu_item">
+        <a href="#" class="">Zapisane</a>
+      </li>
+      <li class="profile_menu_item">
+        <a href="#" class="">Polubione</a>
+      </li>
+      <li class="profile_menu_item">
+        <a href="/personal-data" class="">Profil</a>
+      </li>
+      <li class="profile_menu_item">
+        <a href="#" class="">Jak dodawać lokacje</a>
+      </li>
+      <hr>
+      <li class="profile_menu_item">
+        <a href="#" class="">Regulamin</a>
+      </li>
+      <li class="profile_menu_item">
+        <a href="#" class="">Pomoc</a>
+      </li>
+      <hr>
+      <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" style="all: unset; cursor: pointer; color: #333333">Wyloguj</button>
+      </form>
+    </ul>
   </div>
 </div>
 
@@ -17,7 +38,6 @@
   
   // Ukryj menu po kliknięciu poza obszar
   document.addEventListener('click', function(event) {
-    console.log("cyce")
     var profilePhoto = document.getElementById('user_profile_photo');
     var menu = document.getElementById('profile_menu');
     if (!profilePhoto.contains(event.target)) {
