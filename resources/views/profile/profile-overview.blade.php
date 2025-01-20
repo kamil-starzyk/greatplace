eb5d5ef1818045abd7d49eb9b1dc4a3ec721fc48
XesIhma <kamil_starzyk@outlook.com>
2025-01-20 23:46:06 +0100

    added profile picture to profile-overview

--- resources/views/profile/profile-overview.blade.php
+++ resources/views/profile/profile-overview.blade.php
@@ -5,6 +5,16 @@
 
 <h1>Edycja danych</h1>
 
+@php
+$profile_picture = '';
+if ($user->profile_picture)
+{
+  $profile_picture = Storage::url($user->profile_picture);
+}
+
+@endphp
+<img src="{{$profile_picture}}" alt="Zdjęcie profilowe" class="profile-picture">
+
 
 
 @endsection
