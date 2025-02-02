1e33f346acd9e1ecc5581f7ce924e950215dfbac
XesIhma <kamil_starzyk@outlook.com>
2025-02-02 17:06:01 +0100

    empty-places page created, link to add-place works

--- resources/views/profile/empty-places.blade.php
+++ resources/views/profile/empty-places.blade.php
@@ -3,7 +3,13 @@
 
 @section('profile-content')
 
-<h1>Empty places</h1>
+<h1>Twoje miejsca</h1>
+
+<div id="no-places-container">
+  <img src="{{ asset('img\no_places.png') }}" alt="">
+  <span>Nie dodałeś jeszcze żadnego miejsca</span>
+  <a href="add-form" class="btn-round btn-black btn-big"><span>Dodaj miejsce</span></a>
+</div>
 
 
 
